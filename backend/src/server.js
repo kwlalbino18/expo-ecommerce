c56d146c40b184d9b9c02e78761e6a76481986b9
kwlalbino18 <kwlalbino18@gmail.com>
2026-01-20 15:46:59 -0400

    feat: Initialize backend with Clerk, Inngest, and Mongoose user models, and set up the admin frontend with Clerk authentication.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,18 @@ import { ENV } from "./config/env.js";
 import connectDB from "./config/db.js";
 import dns from "node:dns/promises";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 const __dirname = path.resolve();
 dns.setServers(["1.1.1.1"]);
 
+
+app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions: functions }));
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success" })
@@ -24,9 +30,14 @@ if (ENV.NODE_ENV === "development") {
     });
 }
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
-    connectDB();
-});
+const startServer = async () => {
+    app.listen(ENV.PORT, () => {
+        console.log(`Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+        connectDB();
+    });
+
+}
+
+startServer();
 
 
